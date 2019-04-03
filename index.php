dfd46d011eb810d6d99bb1f64cbedb2a96907115
josedlca <jose.dlcruz.ayala@gmail.com>
2019-04-03 18:11:05 -0500

    review section end well just the template html

--- index.php
+++ index.php
@@ -779,6 +779,48 @@
 				</div>
 			</div>
 		</section>
+		<section class="review">
+			<div class="container">
+				<div class="review__topPart">
+					<div class="row">
+						<div class="col-xl-12">
+							<div class="review__topPart--title">
+								<h4>Client Review</h4>
+							</div>
+						</div>
+						<div class="col-xl-12">
+							<div class="review__topPart--desc">
+								<p>Client Saying about us, how Many we have been satisfying our clients</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="review__botPart">
+					<div class="row">
+						<div class="col-xl-12">
+							<div class="review__botPart--name">
+								<p>JOHN DOE</p>
+							</div>
+						</div>
+						<div class="col-xl-12">
+							<div class="review__botPart--charge">
+								<p>Created Head Inc.</p>
+							</div>
+						</div>
+						<div class="col-xl-12">
+							<div class="review__botPart--desc">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eum doloribus ab amet placeat provident saepe vel! Soluta, molestias deleniti debitis sed beatae dolores facilis aspernatur ratione, nostrum omnis excepturi?</p>
+							</div>
+						</div>
+						<div class="col-xl-12">
+							<div class="review__botPart--img">
+								img
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</main>
 </body>
 </html>
