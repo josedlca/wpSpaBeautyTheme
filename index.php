9480a7b0a35b0904f9fad274fb2f0a82740e0e34
josedlca <jose.dlcruz.ayala@gmail.com>
2019-04-03 19:08:04 -0500

    footer end  well just the template html

--- index.php
+++ index.php
@@ -924,6 +924,124 @@
 				</div>
 			</div>
 		</section>
+		<footer class="footer">
+			<div class="container">
+				<div class="footer__corePart">
+					<div class="row">
+						<div class="col-xl-3">
+							<div class="footer__corePart--sign">
+								<div class="row">
+									<div class="col-xl-12">
+										<div class="footer__corePart--sign_img">
+											some img
+										</div>
+									</div>
+									<div class="col-xl-12">
+										<div class="footer__corePart--sign_languge">
+											<label>Languge</label>
+											<select name="" id="">
+												<option value="">English(UK)</option>
+												<option value="">Spanish</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-xl-12">
+										<div class="footer__corePart--sign_btns">
+											<a href="#">SIGN IN</a>
+											<a href="#">SIGN UP</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-3">
+							<div class="footer__corePart--services">
+								<p>OUR SERVICES</p>
+								<ul>
+									<li>Body Message</li>
+									<li>Spa Therapy</li>
+									<li>Hai-Care & Styles</li>
+									<li>Oil Message</li>
+									<li>Body Waxing</li>
+									<li>Skin Care</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-xl-3">
+							<div class="footer__corePart--quick">
+								<p>QUICK LINKS</p>
+								<ul>
+									<li> <a href="#"> Payment Options </a></a></li>
+									<li><a href="#">Fee Schedule</a></li>
+									<li><a href="#">Getting Started</a></li>
+									<li><a href="#">Identity Verification</a></li>
+									<li><a href="#">Card Verification</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-xl-3">
+							<div class="footer__corePart--event">
+								<p>OUR EVENT</p>
+								<div class="row">
+									<div class="col-xl-12">
+										<div class="footer__corePart--event_one">
+											<div class="row">
+												<div class="col-xl-6">
+													<a href="#">10:00 PM</a>
+												</div>
+												<div class="col-xl-6">
+													<div class="footer__corePart--event_one-place">
+														<p>New York City</p>
+														<p>05-24-2019</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-12">
+										<div class="footer__corePart--event_one">
+											<div class="row">
+												<div class="col-xl-6">
+													<a href="#">10:00 PM</a>
+												</div>
+												<div class="col-xl-6">
+													<div class="footer__corePart--event_one-place">
+														<p>New York City</p>
+														<p>05-24-2019</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="footer__lastPart">
+					<div class="row">
+						<div class="col-xl-4">
+							<div class="footer__lastPart--copy">
+								<p>Copyright 2018 Staker. Designed by Theme_US</p>
+								<p>Privacy Policy Terms & Condition</p>
+							</div>
+						</div>
+						<div class="col-xl-2">
+							<div class="footer__lastPart--social">
+								<p>Connect with us</p>
+								<ul>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+									<li><a href="#"></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 	</main>
 </body>
 </html>
