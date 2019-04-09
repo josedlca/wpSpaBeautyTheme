49e71cd9bc9a9ebeb63c8027ce045ed3ec3df7f1
josedlca <jose.dlcruz.ayala@gmail.com>
2019-04-08 22:23:18 -0500

    review part done well just the css3

--- index.php
+++ index.php
@@ -877,7 +877,7 @@
 		<section class="review">
 			<div class="container">
 				<div class="review__topPart">
-					<div class="row">
+					<div class="row t-c">
 						<div class="col-xl-12">
 							<div class="review__topPart--title">
 								<h4>Client Review</h4>
@@ -891,7 +891,7 @@
 					</div>
 				</div>
 				<div class="review__botPart">
-					<div class="row">
+					<div class="row t-c">
 						<div class="col-xl-12">
 							<div class="review__botPart--name">
 								<p>JOHN DOE</p>
@@ -904,7 +904,7 @@
 						</div>
 						<div class="col-xl-12">
 							<div class="review__botPart--desc">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eum doloribus ab amet placeat provident saepe vel! Soluta, molestias deleniti debitis sed beatae dolores facilis aspernatur ratione, nostrum omnis excepturi?</p>
+								<p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eum doloribus ab amet placeat provident saepe vel! Soluta, molestias deleniti debitis sed beatae dolores facilis aspernatur ratione, nostrum omnis excepturi?</p>
 							</div>
 						</div>
 						<div class="col-xl-12">
